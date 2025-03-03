6c1b349ba317bef8b92f940bb8321277f57c541f
Noura Arafa <nouraarafa180@gmail.com>
2025-03-04 01:41:09 +0200

    after last fix

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,13 +1,20 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn , Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const _Router = inject(Router);
-  if( localStorage.getItem('UserToken') !== null ){
-    return true;
+  const _PLATFORM_ID = inject(PLATFORM_ID);
+
+  if(isPlatformBrowser(_PLATFORM_ID)) {
+
+    if( localStorage.getItem('UserToken') !== null ){
+      return true;
+    }
+    else{
+      _Router.navigate(['/login']);
+      return false;
+    }
   }
-  else{
-    _Router.navigate(['/login']);
-    return false;
-  }
+  return true
 };
